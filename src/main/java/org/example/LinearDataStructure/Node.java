fd80faaad6a67e2e38952ac67a77faa7cb09bae2
Maria Vasilieva <51649407+MariaVasilieva@users.noreply.github.com>
2024-11-01 17:27:21 +0200

    Create package

--- src/main/java/org/example/LinearDataStructure/Node.java
+++ src/main/java/org/example/LinearDataStructure/Node.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.LinearDataStructure;
 
 public class Node <T>{
     T data;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,7 @@
 package org.example;
 
-import org.example.HashTable.HashTable;
-
-import java.util.Objects;
-import java.util.Scanner;
-
 public class Main {
-//    public static void main(String[] args) throws Exception {
-//        Student student1 = new Student("John Doe1", 21);
-//        Student student2 = new Student("John Doe2", 22);
-//        Student student3 = new Student("John Doe3", 23);
-//        Student student4 = new Student("John Doe4", 24);
-//        Student student5 = new Student("John Doe5", 25);
-//        HashTable<Integer,Student> hashTable = new HashTable<>();
-//        hashTable.put(1,student1);
-//        hashTable.put(11,student2);
-//        hashTable.put(21,student3);
-//        hashTable.put(31,student4);
-//        hashTable.put(41,student5);
-//
-//        hashTable.remove(41);
-//    }
+    public static void main(String[] args) throws Exception {
+
+    }
 }
